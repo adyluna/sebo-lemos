cfc5ccc81ce51f679738c7a647a66647eda981e6
adyluna <adylunaleite@gmail.com>
2022-12-18 11:04:29 -0300

    Setei a database para sebo

--- app/backend/src/models/connection.ts
+++ app/backend/src/models/connection.ts
@@ -4,9 +4,14 @@ import 'dotenv/config';
 
 const MONGO_DB_URL = 'mongodb://root:123456@db';
 
+const connectionpOptions = {
+  dbName: 'sebo',
+  useUnifiedTopology: true,
+}
+
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
+) => mongoose.connect(mongoDatabaseURI, connectionpOptions);
 
 export default connectToDatabase;
