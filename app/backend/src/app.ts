46c1a1752b6d5494cfffd1f1e024148555456540
adyluna <adylunaleite@gmail.com>
2022-12-18 16:27:08 -0300

    Funcionando frontend com comunicacao com o backend, CORS implementado

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,6 +5,13 @@ import routes from './routes/Routes';
 
 const app = express();
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(routes);
 app.use(httpErrorMiddleware);
 
--- app/frontend/src/pages/Books.jsx
+++ app/frontend/src/pages/Books.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { requestData } from '../services/requests';
 
-class Books extends React.Component {
-  render() {
-    return (
-      <h1>Livros</h1>
+const Books = () => {
+
+  const [books, setBooks] = useState();
+
+  useEffect(() => {
+    const endpoint = '/allbooks'
+    requestData(endpoint).then((response) => {
+      setBooks(response);
+    }).catch((error) => console.log(error));
+  }, []);
+
+  const showBooks = books && books.map((book) => <h1>{ book.name }</h1>)
+
+  return (
+      showBooks
     );
-  }
 }
 
 export default Books;
