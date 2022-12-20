b9d0f32b4645d2cb54139a86dbccffc8fd910522
adyluna <adylunaleite@gmail.com>
2022-12-20 14:57:03 -0300

    CORS update

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -6,8 +6,11 @@ import routes from './routes/Routes';
 const app = express();
 app.use(express.json());
 
+const RAILWAY = "https://sebo-frontend-production.up.railway.app";
+const LOCAL = "https://localhost:3001"
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://sebo-frontend-production.up.railway.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", RAILWAY || LOCAL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
