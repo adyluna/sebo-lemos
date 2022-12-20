742659e5e58e2b3146241f9b2f99fe5bb662fe82
adyluna <adylunaleite@gmail.com>
2022-12-20 12:42:00 -0300

    Alterando CORS 2

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "sebo-frontend-production.up.railway.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://sebo-frontend-production.up.railway.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
