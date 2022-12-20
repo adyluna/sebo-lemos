3d97c0308d3e162c2d7f6c772a7381edbeed7fc3
adyluna <adylunaleite@gmail.com>
2022-12-20 18:32:33 -0300

    Att

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -8,7 +8,6 @@ app.use(express.json());
 
 const RAILWAY = "https://sebo-frontend-production.up.railway.app";
 const LOCAL = "http://localhost:3000";
-// entre!
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", LOCAL); // update to match the domain you will make the request from
