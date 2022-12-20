c0a2131ac3d5233e9f78f828dfed109c06340ac1
adyluna <adylunaleite@gmail.com>
2022-12-20 15:04:57 -0300

    Duas strings em uma const

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -6,13 +6,12 @@ import routes from './routes/Routes';
 const app = express();
 app.use(express.json());
 
-const RAILWAY = "https://sebo-frontend-production.up.railway.app";
-const LOCAL = "https://localhost:3000";
+const RAILWAY = "https://sebo-frontend-production.up.railway.app" || "https://localhost:3000";
 
 // entre!
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", RAILWAY || LOCAL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", RAILWAY); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
