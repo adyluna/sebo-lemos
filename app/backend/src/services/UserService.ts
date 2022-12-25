0681bbabc31dfe27b0a531b157889d9e7b5fbae6
adyluna <adylunaleite@gmail.com>
2022-12-24 21:13:36 -0300

    Implementado checagem se usuário já existente

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,6 +1,7 @@
 import User from "../domain/User";
 import { IUser } from "../interfaces/IUser";
 import UserODM from "../models/UserODM";
+import HttpException from "../utils/HttpException";
 
 class UserService {
 
@@ -17,13 +18,6 @@ class UserService {
     return null;
   }
 
-  public async createUser(user: IUser) {
-    const userODM = new UserODM();
-    const newUser = await userODM.insert(user);
-
-    return this.createUserDomain(newUser);
-  }
-
   public async findUser(email: string): Promise<User | null> {
     const userODM = new UserODM();
     const result = await userODM.find(email);
@@ -32,6 +26,18 @@ class UserService {
     }
 
     return null
+  }
+
+  public async createUser(user: IUser) {
+    const userODM = new UserODM();
+
+    const userAlreadyExists = await this.findUser(user.email)
+    if (userAlreadyExists) {
+      throw new HttpException(401, 'User already exists');
+    }
+
+    const newUser = await userODM.insert(user);
+    return this.createUserDomain(newUser);
   }
 }
 
