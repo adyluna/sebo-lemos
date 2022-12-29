73660dd330b482adece90bd601c15ab50e0380bd
adyluna <adylunaleite@gmail.com>
2022-12-29 18:29:52 -0300

    Pausa descanso

--- app/frontend/src/components/Book.jsx
+++ app/frontend/src/components/Book.jsx
@@ -7,8 +7,8 @@ const Book = ({ name, author, genre, condition, price }) => {
   // const imageURL = 'https://sebo-backend-production.up.railway.app/images/test01';
 
   const truncate = (str) => {
-    if (str.length > 20) {
-      const slicedString = str.slice(0, 20) + '...';
+    if (str.length > 40) {
+      const slicedString = str.slice(0, 40) + '...';
       return slicedString;
     }
     return str;
@@ -18,7 +18,7 @@ const Book = ({ name, author, genre, condition, price }) => {
     <Row xs={2} className="p-1 m-4 align-items-center justify-content-center bg-warning rounded">
       <Image className="w-50 h-50 p-2" src={logo} alt='test-image'/>
       <Col fluid style={{ fontSize: 10 }} className="p-1">
-      <p className="m-2" style={{ fontSize: 13 }}><strong>{ truncate(name) }</strong></p>
+      <p className="m-2" style={{ fontSize: 13 }}><strong>{ name }</strong></p>
       <p className="m-1">Autor: { author }</p>
       <p className="m-1">{ genre }</p>
       <p className="m-1">{ condition }</p>
