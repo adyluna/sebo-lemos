5b867140e1b8a6ad0d9e6609690fe4aaa3f7f597
adyluna <adylunaleite@gmail.com>
2023-01-03 18:57:29 -0300

    backend: cors: changed access control allow headers to *

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -13,7 +13,8 @@ const LOCAL = "http://localhost:3000";
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", LOCAL);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
