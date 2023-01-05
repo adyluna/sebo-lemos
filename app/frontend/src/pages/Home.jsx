51acf7c37491740b686528f7f73ccd9fef5a8552
adyluna <adylunaleite@gmail.com>
2023-01-05 18:34:37 -0300

    frontend: home page: just some padding and margin

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -20,8 +20,8 @@ const Home = ({allBooks, addProductToCart}) => {
   if (highlightBooks.length === 0) return <Loading />
 
   return (
-    <Container fluid>
-      <h3>Destaques</h3>
+    <Container className='p-3'>
+      <h3 className='mt-3 mb-4'>Destaques</h3>
       <Row xs={1} sm={1} md={2} lg={3} xl={4} className="align-items-center justify-content-center">
         {
           highlightBooks.slice(1,4).map((book, index) =>
@@ -33,7 +33,7 @@ const Home = ({allBooks, addProductToCart}) => {
           )
         }
       </Row>
-      <h3>Novidades</h3>
+      <h3 className='mb-4'>Novidades</h3>
       <Row xs={1} sm={1} md={2} lg={3} xl={4} className="align-items-center justify-content-center">
       {
         highlightBooks.slice(highlightBooks.length - 3, highlightBooks.length).map((book, index) =>
@@ -45,32 +45,32 @@ const Home = ({allBooks, addProductToCart}) => {
         )
         }
       </Row>
-      <h3>Relacionado</h3>
+      <h3 className='mb-4'>Relacionado</h3>
       <Row md={2} sm={1} xs={1} className="align-items-center justify-content-center p-3">
-        <Col className='p-2'>
-        <div className="card flex-md-row mb-4 box-shadow h-md-250 align-items-center justify-content-center">
+        <Col>
+        <div className="p-2 card flex-md-row mb-4 box-shadow h-md-250 align-items-center justify-content-center">
           <div className="card-body d-flex flex-column align-items-start">
-            <strong className="d-inline-block mb-2 text-primary">World</strong>
+            <strong className="d-inline-block mb-2 text-primary">Mundo</strong>
             <h3 className="mb-0">
-              <a className="text-dark" href="about">Featured post</a>
+              <a className="text-dark" href="about">Sebo Lemos</a>
             </h3>
             <div className="mb-1 text-muted">Nov 12</div>
-            <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="about">Continue reading</a>
+            <p className="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="about">Continue lendo</a>
           </div>
           <img src={seboLogo} alt="sebo-logo" className="w-50 h-50 p-2"/>
           </div>
         </Col>
-        <Col className='p-2'>
-        <div className="card flex-md-row mb-4 box-shadow h-md-250 align-items-center justify-content-center">
+        <Col>
+        <div className="p-2 card flex-md-row mb-4 box-shadow h-md-250 align-items-center justify-content-center">
           <div className="card-body d-flex flex-column align-items-start">
-            <strong className="d-inline-block mb-2 text-primary">World</strong>
+            <strong className="d-inline-block mb-2 text-primary">Mundo</strong>
             <h3 className="mb-0">
-              <a className="text-dark" href="about">Featured post</a>
+              <a className="text-dark" href="about">Sebo Lemos</a>
             </h3>
             <div className="mb-1 text-muted">Nov 12</div>
-            <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="about">Continue reading</a>
+            <p className="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="about">Continue lendo</a>
           </div>
           <img src={seboLogo} alt="sebo-logo" className="w-50 h-50 p-2"/>
           </div>
