8b00c96e20f84ae4cba53537a4aa6d03feadc7d1
adyluna <adylunaleite@gmail.com>
2023-01-08 18:03:36 -0300

    frontend: book component: fixed the text position and changed the divs of the component to use the bootstrap card component

--- app/frontend/src/components/Book.jsx
+++ app/frontend/src/components/Book.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { requestImage } from "../services/requests";
-import { Col, Row, Button, ButtonGroup, Container } from 'react-bootstrap';
+import { Row, Button, ButtonGroup, Container, Card } from 'react-bootstrap';
 
 const Book = ({book, addProductToCart}) => {
   const [bookImage, setBookImage] = useState(null);
@@ -16,17 +16,19 @@ const Book = ({book, addProductToCart}) => {
   }, []);
 
   return (
-    <Container className="mb-5">
-    <Row xs={2} style={{ backgroundColor: '#C9CDEC' }} className="p-1 m-1 align-items-center justify-content-center rounded">
-      { bookImage }
-      <Col style={{ fontSize: 10, width: 50, height: 50 }} className="p-1">
-      <p className="m-2" style={{ fontSize: 13 }}><strong>{ name }</strong></p>
-      <p className="m-1">Autor: { author }</p>
-      <p className="m-1">{ genre }</p>
-      <p className="m-1">{ condition }</p>
-      { price && <p style={{ fontSize: 14 }} className="m-0">R$ { price },00</p> }
-      </Col>
-    </Row>
+    <Container className="m-3">
+      <Card style={{ backgroundColor: '#C9CDEC' }} className="mb-2">
+        <Row xs={2} className="p-1 m-1 align-items-center justify-content-center">
+          { bookImage }
+          <Card.Body style={{ fontSize: 10, width: 50, height: 50 }} className="p-1">
+          <Card.Title className="m-2" style={{ fontSize: 13 }}>{ name }</Card.Title>
+          <p className="m-1">Autor: { author }</p>
+          <p className="m-1">{ genre }</p>
+          <p className="m-1">{ condition }</p>
+          { price && <p style={{ fontSize: 14 }} className="m-0">R$ { price },00</p> }
+          </Card.Body>
+        </Row>
+    </Card>
     <Row xs={2} className="align-items-center justify-content-center">
     <ButtonGroup size="sm" className="align-items-center justify-content-center">
       <Button style={{ backgroundColor: '#3A095B' }}>Detalhes</Button>
