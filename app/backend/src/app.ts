b73a797e7947b8a6196c3d6e35a85e3f2bf45bcd
adyluna <adylunaleite@gmail.com>
2023-02-03 15:11:54 -0300

    att railway up

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -11,6 +11,8 @@ app.use(express.json());
 const RAILWAY = "https://sebo-frontend-production.up.railway.app";
 const LOCAL = "http://localhost:3000";
 
+// Entrei!
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", RAILWAY);
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
